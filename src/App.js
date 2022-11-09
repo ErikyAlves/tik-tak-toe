4d795216c5c1ca960655e87b86bbcfcb88e84f02
Eriky Alves <erikyalves3k@gmail.com>
2022-11-09 10:31:44 -0300

    adicionado estilos da instrodução

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 // styles
-import "./styles/css/style.css"
+import "./styles/css/style.css";
+import introIconX from "./img/icons-X/introduction/close.svg";
+import introIconO from "./img/icons-O/introduction/panorama_fish_eye.svg";
 
 export default function App() {
   return (
     <>
-      <h1>Olá mundo</h1>
+      <section id="introduction">
+        <h1>TIK-TAK-TOE</h1>
+        {/* introdução */}
+        <section className="chosen-players">
+          <h2>ESCOLHA COM QUAL QUER JOGAR</h2>
+          <button id="player1">
+            <img src={introIconX} alt="icon X" />
+          </button>
+          <button id="player2">
+            <img src={introIconO} alt="icon X" />
+          </button>
+        </section>
+        {/* game */}
+        <section id="game-board" className="">
+          
+        </section>
+      </section>
     </>
   );
 }
